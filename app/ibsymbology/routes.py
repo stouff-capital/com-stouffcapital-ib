c0ab920d094e36e5d76cfb4c708f318de4806eaf
Gregory Chevalley <gregory.chevalley@gmail.com>
2021-12-31 09:10:40 +0100

    symbology option checks

--- app/ibsymbology/routes.py
+++ app/ibsymbology/routes.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+import datetime
 from flask import render_template, flash, redirect, url_for, request, g, jsonify, current_app
 from app import db
 from app.models import Ibcontract, Ibexecutionrestful, Ibsymbology
 from app.ibsymbology import bp
+import pandas as pd
 
 from app.bridge import routes as routes_bridge
 
@@ -252,3 +253,63 @@ def remove_mapping(conid):
         return jsonify({'status': 'ok', 'input': conid, 'controller': 'ibsymbology'})
     else:
         return jsonify( {'status': 'error', 'error': 'missing entry in database', 'inputData': conid, 'controller': 'ibsymbology'} )
+
+
+@bp.route('/ibsymbology/check-new-assets', methods=['GET'])
+def check_new_assets():
+
+    current_app.logger.info(f'retrieve new symbology after: {(datetime.datetime.now() + datetime.timedelta(days=-5)).strftime("%Y-%m-%d")}')
+    result = new_manual_mapping( f'{(datetime.datetime.now() + datetime.timedelta(days=-5)).strftime("%Y-%m-%d")}' ).get_json()
+    current_app.logger.info(f'new asset(s) {len(result["contracts"])}')
+
+    data = []
+    for newAsset in result['contracts']:
+        for subkey_ibcontract in newAsset['ibcontract']:
+            newAsset["ibcontract_" + subkey_ibcontract] = newAsset['ibcontract'][subkey_ibcontract] #flatten
+        del newAsset['ibcontract']
+        data.append(newAsset)
+
+    df = pd.DataFrame(data)
+
+    current_app.logger.info(f'df new asset(s) {len(df)}')
+
+    df['bbgTicker_underlying'] = df.ticker.str.extract(r'^([A-Za-z0-9]+)\s')
+    df['ib_underlying'] = df.ibcontract_symbol.str.extract(r'^(.+)\s[\d]+')
+    
+    df.ibcontract_expiry = pd.to_datetime( df.ibcontract_expiry, format="%a, %d %b %Y %H:%M:%S %Z" )
+    df['ibExpiryPart'] = df.ibcontract_expiry.dt.strftime("%m/%d/%y")
+    
+    # options with expiry at the open are t-1 in ib
+    df['ibExpiryPart_alt'] = (df.ibcontract_expiry + pd.Timedelta('1 days')).dt.strftime("%m/%d/%y")
+    
+    df['bbgTicker_expiry'] = df.ticker.str.extract(r'([\d]{2}/[\d]{2}/[\d]{2})')
+
+    df['bbgTicker_putCall'] = df.ticker.str.extract(r'\s([PpCp])[\d]+')
+
+    df['bbgTicker_strike'] = df.ticker.str.extract(r'\s[PpCp]([\d\.]+)')
+    df['bbgTicker_strike'] = pd.to_numeric(df['bbgTicker_strike'])
+    
+    h_options = ['ibcontract_conid', 'ibcontract_symbol', 'ibcontract_currency', 'ibcontract_putCall', 'ticker', 'ibExpiryPart', 'bbgTicker_expiry', 'ibcontract_strike', 'bbgTicker_strike', 'bbgTicker_putCall']
+    
+    df_options = df[df.ibcontract_assetCategory == 'OPT']
+    
+    df_options[ df_options['bbgTicker_underlying'].str.strip() != df_options['ib_underlying'].str.strip() ][['bbgTicker_underlying', 'ib_underlying'] + h_options] #not used
+    
+    df_sameExpiryDate = df_options[ (df_options.ibExpiryPart != df_options.bbgTicker_expiry) & (df_options.ibExpiryPart_alt != df_options.bbgTicker_expiry)  ][h_options]
+
+    df_putCall = df_options[df_options.ibcontract_putCall != df_options.bbgTicker_putCall][h_options]
+
+    df_strike = df_options[df_options.ibcontract_strike != df_options.bbgTicker_strike][h_options]
+
+    return jsonify({
+        'status': 'ok', 
+        'controller': 'ibsymbology',
+        'timestamp': datetime.datetime.now().isoformat(),
+        'size': len(df),
+        '?sameexpirydate': df_sameExpiryDate.to_dict(orient='records'),
+        '?putcall': df_putCall.to_dict(orient='records'),
+        '?strike': df_strike.to_dict(orient='records'),
+    })
+
+
+
